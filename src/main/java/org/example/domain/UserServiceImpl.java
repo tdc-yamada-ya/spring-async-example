78999fcd8025aac98ce79b9434d5c9fd04560a66
Yasuhiro Yamada <yamada_ya@tdc.co.jp>
2023-10-23 10:43:33 +0900

    u

--- src/main/java/org/example/domain/UserServiceImpl.java
+++ src/main/java/org/example/domain/UserServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,6 +21,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public BatchCreateUsersTask batchCreateUsers(int count) {
         BatchCreateUsersTask savedTask = batchCreateUsersTaskRepository.save(new BatchCreateUsersTask(null, "started"));
         userBatchCreator.createUsers(savedTask, count);
